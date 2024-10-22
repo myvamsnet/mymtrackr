d2d43285498140b843d517756f0184218ea62be8
myvamsnet <myvamsnet@gmail.com>
2024-10-22 10:05:13 +0100

    Worked on update

--- src/app/(protectedRoute)/app/records/[type]/components/RecordTypeLists.tsx
+++ src/app/(protectedRoute)/app/records/[type]/components/RecordTypeLists.tsx
@@ -1,19 +1,18 @@
-'use client';
-import { RecordItem } from '@/app/(protectedRoute)/app/_components/common/records/RecordItem';
-import { RecordsNotFound } from '@/app/(protectedRoute)/app/_components/common/records/RecordsNotFound';
-import { LoadingRecords } from '@/components/LoadingRecords';
-import { useGetRecordByType } from '@/hooks/useGetRecordByType';
-import { RecordData } from '@/types/records';
-import React from 'react';
+"use client";
+import { RecordItem } from "@/app/(protectedRoute)/app/_components/common/records/RecordItem";
+import { RecordsNotFound } from "@/app/(protectedRoute)/app/_components/common/records/RecordsNotFound";
+import { LoadingRecords } from "@/components/LoadingRecords";
+import { useGetRecordByType } from "@/hooks/useGetRecordByType";
+import React from "react";
 
 export const RecordTypeLists = () => {
   const { records, status, error } = useGetRecordByType();
 
   return (
     <section className=" bg-off-white-300 overflow-y-auto overflow-x-hidden ">
-      {status === 'error' && <p>{error?.name}</p>}
+      {status === "error" && <p>{error?.name}</p>}
       {records && records?.length === 0 && <RecordsNotFound />}
-      {status === 'pending' && <LoadingRecords />}
+      {status === "pending" && <LoadingRecords />}
       {records &&
         records?.length > 0 &&
         records?.map((record, index) => (
