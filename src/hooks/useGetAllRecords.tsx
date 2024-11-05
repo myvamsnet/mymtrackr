75987d360b8529503c4550df6266b4a2e44e0681
myvamsnet <myvamsnet@gmail.com>
2024-11-05 11:42:22 +0100

    Worked on bug issues

--- src/hooks/useGetAllRecords.tsx
+++ src/hooks/useGetAllRecords.tsx
@@ -1,43 +1,43 @@
-'use client';
-import { Records } from '@/types/records';
-import { useQuery } from '@tanstack/react-query';
-import { sortArray } from '@/lib/helper/sortData';
-import dayjs from 'dayjs';
-import { useSearchParams } from 'next/navigation';
-import axiosInstance from '@/lib/axios';
+"use client";
+import { Records } from "@/types/records";
+import { useQuery } from "@tanstack/react-query";
+import { sortArray } from "@/lib/helper/sortData";
+import dayjs from "dayjs";
+import { useSearchParams } from "next/navigation";
+import axiosInstance from "@/lib/axios";
 
 export const useGetAllRecords = () => {
   const searchParams = useSearchParams();
-  const yearlyRange = searchParams.get('year');
-  const monthlyRange = searchParams.get('month');
-  const searchTerm = searchParams.get('searchTerm');
+  const yearlyRange = searchParams.get("year");
+  const monthlyRange = searchParams.get("month");
+  const searchTerm = searchParams.get("searchTerm");
 
   const startDate = dayjs(monthlyRange)
     .year(Number(yearlyRange))
-    .format('YYYY-MM-DD');
+    .format("YYYY-MM-DD");
 
   const endDate = dayjs(yearlyRange)
     .month(Number(monthlyRange))
-    .add(1, 'day')
-    .format('YYYY-MM-DD');
+    .add(1, "day")
+    .format("YYYY-MM-DD");
   const values = {
     startDate,
     endDate,
     searchTerm,
   };
   const { data, isLoading, error, status } = useQuery<RecordsResponse>({
-    queryKey: ['records', searchTerm ?? '', values ?? ''],
+    queryKey: ["records", searchTerm ?? "", values ?? ""],
     queryFn: async () => {
       const param = new URLSearchParams(Object(values)).toString();
 
       const { data } = await axiosInstance.get(
-        `/records/${param ? `?${param}` : ''}`
+        `/records/${param ? `?${param}` : ""}`
       );
       return data;
     },
   });
 
-  const records = data?.data && sortArray(data?.data, 'updated_at');
+  const records = data?.data && sortArray(data?.data, "updated_at");
 
   return {
     records,
