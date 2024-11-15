fbcbd8fff3a2a617f9908101322afd9d9dbbbdd2
myvamsnet <myvamsnet@gmail.com>
2024-11-15 16:24:55 +0100

    Fixed issues on Layout

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -6,11 +6,10 @@ export async function generateMetadata({
   searchParams,
 }: GenerateMetadataProps) {
   const referralCode = searchParams.referralCode || null;
-
   return {
     title: referralCode
       ? `Join us with referral code: ${referralCode}`
-      : "Simplify Your Financial Management with Mtrackr | Register Page",
+      : "I use Mtrackr for records keeping  and earn high interests on my referral. Join me on Mtrackr  and get ₦2000 on referral!",
     description: referralCode
       ? "I use Mtrackr for records keeping  and earn high interests on my referral. Join me on Mtrackr  and get ₦2000 on referral!"
       : "Say goodbye to someone must be stealing my money, stress and the Hassles of Traditional Money Management. Mtrackr allows you to effortlessly manage money with confidence and ease.",
@@ -18,7 +17,6 @@ export async function generateMetadata({
 }
 
 const Register = () => {
-  // console.log(searchParams);
   return (
     <AuthLayout
       title="Register"
