47ab782169f538d8aaec3f4604ca15c9e28a6674
olatubora-felix <olatubora1996@gmail.com>
2025-02-13 15:59:17 +0100

    Worked on count down

--- src/app/(auth)/components/RegisterForm.tsx
+++ src/app/(auth)/components/RegisterForm.tsx
@@ -5,44 +5,52 @@ import { inputLists } from "@/constant/auth";
 import { useSignUp } from "../hook/useSignUp";
 import Link from "next/link";
 import React from "react";
+import { CountdownModal } from "@/components/CountdownModal";
 export const RegisterForm = () => {
-  const { control, handleSubmit, onSubmit, isPending } = useSignUp();
-
+  const { control, handleSubmit, onSubmit, isPending, isOpen, toggle, live } =
+    useSignUp();
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="grid gap-4 py-2"
-    >
-      {inputLists.map((input) => (
-        <CustomInput
-          key={input.name}
-          name={input.name}
-          type={input.type}
-          label={input.label}
-          control={control}
-          placeholder={input.placeholder}
-        />
-      ))}
-      <small className="md:mt-0 mt-4">
-        By continuing you agree to our{" "}
-        <Link
-          href={"https://www.myvamsnet.com/terms-of-service"}
-          target="_blank"
-          className="text-primary text-sm font-normal"
-        >
-          Terms of Service
-        </Link>
-      </small>
-      <Button
-        type="submit"
-        className={`w-full  h-[52px] text-base font-normal ${
-          isPending ? "opacity-55 cursor-not-allowed" : ""
-        }`}
-        disabled={isPending}
-      >
-        {isPending ? "Loading..." : "Sign Up"}
-      </Button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)} className="grid gap-4 py-2">
+        {inputLists.map((input) => (
+          <CustomInput
+            key={input.name}
+            name={input.name}
+            type={input.type}
+            label={input.label}
+            control={control}
+            placeholder={input.placeholder}
+          />
+        ))}
+        <small className="md:mt-0 mt-4">
+          By continuing you agree to our{" "}
+          <Link
+            href={"https://www.myvamsnet.com/terms-of-service"}
+            target="_blank"
+            className="text-primary text-sm font-normal"
+          >
+            Terms of Service
+          </Link>
+        </small>
+        {live === "not-live" ? (
+          <CountdownModal
+            onComplete={() => console.log("It's February 24th!")}
+            isOpen={isOpen}
+            toggle={toggle}
+          />
+        ) : (
+          <Button
+            type="submit"
+            className={`w-full  h-[52px] text-base font-normal ${
+              isPending ? "opacity-55 cursor-not-allowed" : ""
+            }`}
+            disabled={isPending}
+          >
+            {isPending ? "Loading..." : "Sign Up"}
+          </Button>
+        )}
+      </form>
+    </>
   );
 };
 
