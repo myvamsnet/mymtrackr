0de9b440ca7fd5a7067b7d8e845616cd9a622daa
olatubora-felix <olatubora1996@gmail.com>
2025-02-17 14:21:25 +0100

    Fixed settings isuues

--- src/app/(protectedRoute)/settings/business/__components/BusinessProfile.tsx
+++ src/app/(protectedRoute)/settings/business/__components/BusinessProfile.tsx
@@ -7,16 +7,12 @@ import { BusinessData } from "@/types/business";
 export const BusinessProfile = ({
   previewUrl,
   handleFileChange,
-  user,
+  userName,
 }: Props) => {
   return (
     <section className="py-4 flex justify-center items-center bg-off-white-300">
       <div className=" relative w-[120px]">
-        <CustomAvatar
-          name={user?.businessName ?? "My MTracker"}
-          size={120}
-          imgUrl={previewUrl}
-        />
+        <CustomAvatar name={userName} size={120} imgUrl={previewUrl} />
         <label
           className=" absolute bottom-0 right-0 cursor-pointer"
           id="file"
@@ -37,5 +33,5 @@ export const BusinessProfile = ({
 interface Props {
   handleFileChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   previewUrl: string;
-  user: BusinessData;
+  userName: string;
 }
